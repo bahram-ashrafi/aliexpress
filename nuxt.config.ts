062d05642c98da6c56f95610763ee4fde8236b11
Bahram Ashrafi <bahram.ashrafi@gmail.com>
2023-08-31 01:18:30 +0330

    [INI] config nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import {persistedState} from "@pinia-plugin-persistedstate/nuxt/dist/runtime/storages";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    pages: true,
+    modules: [
+        'nuxt-icon',
+        'nuxt-lodash',
+        '@pinia/nuxt',
+        '@pinia-plugin-persistedstate/nuxt',
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/supabase'
+    ],
+    runtimeConfig: {
+        public: {
+            stripePK: process.env.STRIPE_PK_KEY
+        }
+    },
+    app: {
+        head: {
+            script: [
+                {src: 'https://js/stripe.com/v3/', defer: true}
+            ]
+        }
+    }
 })
